4d2ff9c325c757738992f94bdcc575a1897a05aa
Michel Moriniaux <MichelMoriniaux@users.noreply.github.com>
2026-07-01 19:56:00 -0700

    bump version number

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -8,8 +8,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCompany("Michel Moriniaux")]
 [assembly: AssemblyProduct("NINA.Plugin.OnStepXTools")]
 [assembly: AssemblyCopyright("Copyright © 2026")]
-[assembly: AssemblyVersion("1.0.1.3")]
-[assembly: AssemblyFileVersion("1.0.1.3")]
+[assembly: AssemblyVersion("1.0.1.4")]
+[assembly: AssemblyFileVersion("1.0.1.4")]
 [assembly: ComVisible(false)]
 
 // NINA metadata - MinimumApplicationVersion is a load-time gate; must match the Plugins subfolder name
